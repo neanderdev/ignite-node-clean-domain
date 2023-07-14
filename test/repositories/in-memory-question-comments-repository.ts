92a346520ec82c7086ede05f354bb2ac6da48aa4
Neander de Souza <neander.faria@hotmail.com>
2023-07-14 16:46:20 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,5 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -18,6 +20,14 @@ export class InMemoryQuestionCommentsRepository
     }
 
     return questionComment
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async delete(questionComment: QuestionComment) {
